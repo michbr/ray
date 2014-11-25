7551ed7b546cc7d6c422b7880e2bc1563c07b5f5
Cullen <culleneason@gmail.com>
2014-11-25 16:53:22 -0700

    Added first hello world attempt

--- draco/cegui/test.cc
+++ draco/cegui/test.cc
@@ -7,12 +7,23 @@
 
 
 using namespace std;
-//using namespace CEGUI;
+using namespace CEGUI;
 
 int main(int argc, char **argv) {
 	
 	cout << "HI!" << endl;
 	
+	WindowManager &window = WindowManager::getSingleton();
+	Window *root = window.createWindow("DefaultWindow", "root");
+	System::getSingleton().setGUISheet(root);
+	
+	FrameWindow *frame = (FrameWindow *)window.createWindow("TaharezLook/FrameWindow", "testWindow");
+	root->addChildWindow(frame);
+	
+	frame->setPosition(UVector2(UDim(0.25f, 0), UDIM(0.25f, 0)));
+	frame->setSize(UVector2(UDim(0.5f, 0), UDIM(0.5f, 0)))
+	
+	frame->setTest("Hello World!");
 	
 	return 0;
 }
