217f946fce4119f61a7991191769f3b334bfc4c8
Cullen S. Eason <semimono@annapolis.cs.colostate.edu>
2014-11-26 13:48:14 -0700

    Potentially finished hello world.

--- draco/cegui/test.cc
+++ draco/cegui/test.cc
@@ -2,6 +2,9 @@
 
 //#include "CEGUI.h"
 #include "CEGUI/System.h"
+#include "CEGUI/WindowManager.h"
+#include "CEGUI/WindowFactoryManager.h"
+#include "CEGUI/widgets/FrameWindow.h"
 
 #include <iostream>
 
@@ -13,18 +16,20 @@ int main(int argc, char **argv) {
 	
 	cout << "HI!" << endl;
 	
-	WindowManager &window = Singleton<WindowManager>::getSingleton();
-//	WindowManager &window = WindowManager::getSingleton();
+//	WindowManager *window = &Singleton<WindowManager>::getSingleton();
+//	WindowFactoryManager *factoryM = &Singleton<WindowFactoryManager>::getSingleton();
+//	WindowFactory *factory = factoryM->getFactory("DefaultWindow");
+	WindowManager &window = WindowManager::getSingleton();
 	Window *root = window.createWindow("DefaultWindow", "root");
-	System::getSingleton().setGUISheet(root);
+	System::getSingleton().getDefaultGUIContext().setRootWindow(root);
 	
-	FrameWindow *frame = (FrameWindow *)window.createWindow("TaharezLook/FrameWindow", "testWindow");
-	root->addChildWindow(frame);
+	FrameWindow *frame = static_cast<FrameWindow*>(window.createWindow("TaharezLook/FrameWindow", "testWindow"));
+	root->addChild(frame);
 	
-	frame->setPosition(UVector2(UDim(0.25f, 0), UDIM(0.25f, 0)));
-	frame->setSize(UVector2(UDim(0.5f, 0), UDIM(0.5f, 0)))
+	frame->setPosition(UVector2(UDim(0.25f, 0), UDim(0.25f, 0)));
+	frame->setSize(USize(UDim(0.5f, 0), UDim(0.5f, 0)));
 	
-	frame->setTest("Hello World!");
+	frame->setText("Hello World!");
 	
 	return 0;
 }
