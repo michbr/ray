425aa69d209378ab44efb7f9f6e8f8e5cf514420
semimono <culleneason@gmail.com>
2014-12-20 14:02:57 -0700

    Began adding tabs

--- draco/fltk/draco.cc
+++ draco/fltk/draco.cc
@@ -2,8 +2,7 @@
 #include "FL/Fl.H"
 #include "FL/Fl_Window.H"
 #include "FL/Fl_Box.H"
-
-#include "scale.h"
+#include "FL/Fl_Tabs.H"
 
 #include <iostream>
 
@@ -12,18 +11,22 @@ using namespace std;
 
 int main(int argc, char **argv) {
 
-	Fl_Window *window = new Fl_Window(300,180);
-	Fl_Box *box = new Fl_Box(20,40,260,100,"Hello, World!");
-	box->box(FL_UP_BOX);
-	box->labelsize(36);
-	box->labelfont(FL_BOLD+FL_ITALIC);
-	box->labeltype(FL_SHADOW_LABEL);
+	Fl_Window *window = new Fl_Window(300, 180);
+	{
+		Fl_Tabs *tabs = new Fl_Tabs(10, 10, 300, 200);
+		{
+			Fl_Box *box = new Fl_Box(20, 40, 260, 100, "Hello, World!");
+			box->box(FL_UP_BOX);
+			box->labelsize(36);
+			box->labelfont(FL_BOLD + FL_ITALIC);
+			box->labeltype(FL_SHADOW_LABEL);
+		}
+		tabs->end();
+	}
 	window->end();
 	window->show(argc, argv);
 
-	Fl::scheme("gtk+");
-
-	Scale thingy("thingy", "other thingy!");
+	Fl::scheme("plastic");
 
 	int endCode = Fl::run();
 
