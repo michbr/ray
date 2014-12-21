0ff65cdc1cc7a8ca156835976ec10caaa67c8ddd
semimono <culleneason@gmail.com>
2014-12-20 22:12:36 -0700

    Made tabs "work"

--- draco/fltk/draco.cc
+++ draco/fltk/draco.cc
@@ -11,15 +11,21 @@ using namespace std;
 
 int main(int argc, char **argv) {
 
-	Fl_Window *window = new Fl_Window(300, 180);
+	Fl_Window *window = new Fl_Window(640, 480);
 	{
-		Fl_Tabs *tabs = new Fl_Tabs(10, 10, 300, 200);
+		Fl_Tabs *tabs = new Fl_Tabs(10, 10, 620, 440);
 		{
-			Fl_Box *box = new Fl_Box(20, 40, 260, 100, "Hello, World!");
-			box->box(FL_UP_BOX);
-			box->labelsize(36);
-			box->labelfont(FL_BOLD + FL_ITALIC);
-			box->labeltype(FL_SHADOW_LABEL);
+			int x, y, w, h, tabH;
+			tabs->client_area(x, y, w, h, tabH);
+			Fl_Group *tab1 = new Fl_Group(5, 5, tabs->w() -10, tabs->h() -45, "Tab 1");
+			{
+				Fl_Box *box = new Fl_Box(20, 40, 260, 100, "Hello, World!");
+				box->box(FL_UP_BOX);
+				box->labelsize(36);
+				box->labelfont(FL_BOLD + FL_ITALIC);
+				box->labeltype(FL_SHADOW_LABEL);
+			}
+			tab1->end();
 		}
 		tabs->end();
 	}
