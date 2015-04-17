a0aa5b65ac3b31e0e1c333674bcb2786e9f5447f
semimono <culleneason@gmail.com>
2015-04-17 13:12:35 -0600

    Finished implementing new sphere mutator
    Needs testing and debugging

--- draco-utilities/draco/Voxel/mutators/sphere.h
+++ draco-utilities/draco/Voxel/mutators/sphere.h
@@ -12,15 +12,19 @@ namespace Vox {
     public:
 	    SphereMut(const Vector3<double>& pos, double radius, const Voxel& value);
 
-	    bool mutate(const Tree& target, const Index& pos, byte index, Node& parent, void* cache) const;
-	    void getBounds(const Tree& target, Index& min, Index& max, void* cache) const;
+	    bool mutate(Application* app, const Index& pos, byte index, Node& parent) const;
+	    Application* setup(Tree& target) const;
 //	    Mutator::TAction action(const Index& pos) const;
 
     protected:
 	    Voxel value;
 	    Vector3<double> pos;
 	    double radius;
-		double minRadius, minRadSqr, maxRadius, maxRadSqr;
+		
+		class SphereApplication: public Application {
+		public:
+			double minRadius, minRadSqr, maxRadius, maxRadSqr;
+		};
     };
 }
 
--- draco-utilities/draco/Voxel/source/mutators/sphere.cc
+++ draco-utilities/draco/Voxel/source/mutators/sphere.cc
@@ -6,8 +6,7 @@ using namespace std;
 using namespace Vox;
 
 
-SphereMut::SphereMut(const Vector3<double>& pos, double radius, const Voxel& value):
-pos(pos), radius(radius), value(value), minRadius(radius -1), minRadSqr(minRadius *minRadius), maxRadius(radius +1), maxRadSqr(maxRadius *maxRadius) {
+SphereMut::SphereMut(const Vector3<double>& pos, double radius, const Voxel& value): pos(pos), radius(radius), value(value) {
 //	this.value = value;
 //	Vector3 radiusCube = new Vector3(worldRadius, worldRadius, worldRadius) / control.voxelSize();
 //	min = control.transform.InverseTransformPoint(worldPosition) / control.voxelSize() - radiusCube - Vector3.one * (control.voxelSize() / 2);
@@ -16,27 +15,34 @@ pos(pos), radius(radius), value(value), minRadius(radius -1), minRadSqr(minRadiu
 }
 
 
-bool SphereMut::mutate(const Tree& target, const Index& voxelPos, byte index, Node& parent) const {
+bool SphereMut::mutate(Application* app, const Index& voxelPos, byte index, Node& parent) const {
+	SphereApplication* sApp = (SphereApplication*) app;
 	double dis = (pos -Vector3<double>(voxelPos.x, voxelPos.y, voxelPos.z)).magnitude();
-	if (dis > maxDis)
+	if (dis > sApp->maxRadius)
 		return false;
-	if (dis < minDis) {
+	if (dis < sApp->minRadius) {
 		parent.set(index, value);
 		return false;
 	}
-	byte newOpacity = (byte)((parent.getOpacity(index) * (dis - minDis) + value.opacity * (maxDis - dis)) / 2);
-	if ((dis - minDis) > 0.5f)
+	byte newOpacity = (byte)((parent.getOpacity(index) * (dis -sApp->minRadius) + value.opacity * (sApp->maxRadius - dis)) / 2);
+	if ((dis - sApp->minRadius) > 0.5f)
 		parent.set(index, Voxel(value.material, newOpacity));
     else
         parent.set(index, Voxel(parent.getMaterial(index), newOpacity));
     return true;
 }
 
-void SphereMut::getBounds(const Tree& target, Index& min, Index& max) const {
+Mutator::Application* SphereMut::setup(Tree& target) const {
 	Vector3<double> radiusCube = Vector3<double>(radius, radius, radius) / target.voxSize;
 //	cout << "radius: " << radiusCube << endl;
 	Vector3<double> exactMin = (pos -target.pos) /target.voxSize -radiusCube -Vector3<double>(target.voxSize /2, target.voxSize /2, target.voxSize /2);
 	Vector3<double> exactMax = exactMin + radiusCube * 2.0;
-	min = Index(target.maxDepth, exactMin.x, exactMin.y, exactMin.z);
-	max = Index(target.maxDepth, exactMax.x, exactMax.y, exactMax.z);
+	SphereApplication* app = new SphereApplication();
+	app->tree = &target;
+	app->min = Index(target.maxDepth, exactMin.x, exactMin.y, exactMin.z);
+	app->max = Index(target.maxDepth, exactMax.x, exactMax.y, exactMax.z);
+	app->minRadius = radius /target.voxSize -1;
+	app->minRadSqr = app->minRadius *app->minRadius;
+	app->maxRadius = radius /target.voxSize +1;
+	app->maxRadSqr = app->maxRadius *app->maxRadius;
 }
